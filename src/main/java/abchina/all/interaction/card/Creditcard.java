83a8106b72532381fc00220a00ef7e9dd3798c2e
15608447849 <793065165@qq.com>
2017-10-10 16:43:32 +0800

    农业银行数据抓取进行中

---
+++ src/main/java/abchina/all/interaction/card/Creditcard.java
@@ -0,0 +1,24 @@
+package abchina.all.interaction.card;
+
+import abchina.all.AllData;
+import abchina.all.imps.AllDataIns;
+
+/**
+ * Created by user on 2017/10/10.
+ */
+public class Creditcard extends AllDataIns {
+
+    public Creditcard(AllData mData) throws Exception {
+        super(mData);
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
