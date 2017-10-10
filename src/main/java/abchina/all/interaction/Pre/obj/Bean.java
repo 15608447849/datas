e660e0cfcaf9ea35ee4aa24131b8ac54aef795b9
15608447849 <793065165@qq.com>
2017-10-10 16:45:00 +0800

    农业银行数据抓取进行中

---
+++ src/main/java/abchina/all/interaction/Pre/obj/Bean.java
@@ -0,0 +1,61 @@
+package abchina.all.interaction.Pre.obj;
+
+/**
+ * Created by user on 2017/10/10.
+ */
+
+public class Bean {
+    /**
+     * id : 1001
+     * name : 传世之宝”金银元宝1
+     * type : 收藏投资
+     * type_img : img/12.jpg
+     * qr_code : http://www.qq.com
+     */
+
+    private int id;
+    private String name;
+    private String type;
+    private String type_img;
+    private String qr_code;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType_img() {
+        return type_img;
+    }
+
+    public void setType_img(String type_img) {
+        this.type_img = type_img;
+    }
+
+    public String getQr_code() {
+        return qr_code;
+    }
+
+    public void setQr_code(String qr_code) {
+        this.qr_code = qr_code;
+    }
+}
